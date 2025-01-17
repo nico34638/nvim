1001b8f486dc87563c21413fa20cadbf7d06f6ad
Nicolas Fidel <nicolas.fidel@winamax.fr>
2025-01-17 14:10:58 +0100

    lua: coding: change format

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -74,69 +74,69 @@ return {
 			require("nvim-treesitter.configs").setup(opts)
 		end,
 	},
-	{
-		"stevearc/conform.nvim",
-		event = "BufWrite",
-		keys = {
-			{
-				"<leader>ot",
-				function()
-					vim.g.autoformat = not vim.g.autoformat
-					vim.notify(
-						"Auto format " .. (vim.g.autoformat and "enabled" or "disabled"),
-						vim.log.levels.INFO,
-						{ title = "Formatter" }
-					)
-				end,
-				desc = "toggle",
-			},
-			{
-				"<leader>ob",
-				function()
-					local get_or_default = require("utils").get_or_default
-					vim.b.autoformat = not get_or_default(vim.b.autoformat, vim.g.autoformat)
-					vim.notify(
-						"Buffer Auto format " .. (vim.b.autoformat and "enabled" or "disabled"),
-						vim.log.levels.INFO,
-						{ title = "Formatter" }
-					)
-				end,
-				desc = "buffer toggle",
-			},
-			{
-				"<leader>of",
-				function()
-					require("conform").format({ async = true })
-				end,
-				desc = "format buffer",
-			},
-		},
-		opts = {
-			formatters_by_ft = {
-				lua = { "stylua" },
-				c = { "clang-format" },
-				cpp = { "clang-format" },
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				python = { "black" },
-				rust = { "rustfmt" },
-				nix = { "nixpkgs_fmt" },
-				go = { "gofmt" },
-			},
-			default_format_opts = {
-				lsp_format = "never",
-			},
-			format_on_save = function(bufnr)
-				local get_or_default = require("utils").get_or_default
-				if get_or_default(vim.b.autoformat, vim.g.autoformat) then
-					return { timeout_ms = 500 }
-				end
-			end,
-		},
-		init = function()
-			vim.g.autoformat = true
-		end,
-	},
+    {
+        "mhartington/formatter.nvim",
+        event = "BufWrite",
+        keys = {
+            {
+                "<leader>ot",
+                function()
+                    vim.g.autoformat = not vim.g.autoformat
+                    vim.notify(
+                        "Auto format " .. (vim.g.autoformat and "enabled" or "disabled"),
+                        vim.log.levels.INFO,
+                        { title = "Formatter" }
+                    )
+                end,
+                desc = "toggle",
+            },
+            {
+                "<leader>ob",
+                function()
+                    local get_or_default = require("utils").get_or_default
+                    vim.b.autoformat = not get_or_default(vim.b.autoformat, vim.g.autoformat)
+                    vim.notify(
+                        "Buffer Auto format " .. (vim.b.autoformat and "enabled" or "disabled"),
+                        vim.log.levels.INFO,
+                        { title = "Formatter" }
+                    )
+                end,
+                desc = "buffer toggle",
+            },
+            { "<leader>of", "<Cmd>silent! Format<CR>", desc = "format" },
+        },
+        config = function()
+            vim.g.autoformat = true
+
+            local formatter = require("formatter")
+            formatter.setup({
+                filetype = {
+                    lua = { require("formatter.filetypes.lua").stylua },
+                    c = { require("formatter.filetypes.c").clangformat },
+                    cpp = { require("formatter.filetypes.cpp").clangformat },
+                    javascript = { require("formatter.filetypes.javascript").prettier },
+                    typescript = { require("formatter.filetypes.typescript").prettier },
+                    javascriptreact = { require("formatter.filetypes.javascriptreact").prettier },
+                    typescriptreact = { require("formatter.filetypes.typescriptreact").prettier },
+                    json = { require("formatter.filetypes.json").prettier },
+                    yaml = { require("formatter.filetypes.yaml").prettier },
+                    python = { require("formatter.filetypes.python").black },
+                    rust = { require("formatter.filetypes.rust").rustfmt },
+                    nix = { require("formatter.filetypes.nix").nixpkgs_fmt },
+                    go = { require("formatter.filetypes.go").gofmt },
+                },
+            })
+
+            vim.api.nvim_create_autocmd("BufWritePost", {
+                group = vim.api.nvim_create_augroup("my_format_write", { clear = true }),
+                pattern = "*",
+                callback = function()
+                    local get_or_default = require("utils").get_or_default
+                    if get_or_default(vim.b.autoformat, vim.g.autoformat) then
+                        vim.cmd("silent! FormatWrite")
+                    end
+                end,
+            })
+        end,
+    },
 }
