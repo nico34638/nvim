c24c0cb910aebe630ea3a523c4a5817454b6f199
Nicolas Fidel <nicolas.fidel@winamax.fr>
2026-03-30 11:06:08 +0200

    telescope: add focus mode

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -38,6 +38,41 @@ return {
 				{ "<leader>fb", telescope("buffers"), desc = "buffers" },
 				{ "<leader>fc", telescope("colorscheme"), desc = "colorscheme" },
 				{ "<leader>fh", telescope("oldfiles"), desc = "history" },
+				{
+					"<leader>fw",
+					function()
+						local utils = require("utils")
+						require("telescope.builtin").find_files({
+							prompt_title = "Change cwd (root: " .. utils._root_cwd .. ")",
+							cwd = utils._root_cwd,
+							find_command = { "fd", "--type", "d", "--hidden", "--exclude", ".git" },
+							attach_mappings = function(prompt_bufnr)
+								require("telescope.actions").select_default:replace(function()
+									local entry = require("telescope.actions.state").get_selected_entry()
+									require("telescope.actions").close(prompt_bufnr)
+									local dir = entry.path or (utils._root_cwd .. "/" .. entry[1])
+									utils.push_cwd(dir)
+								end)
+								return true
+							end,
+						})
+					end,
+					desc = "change cwd",
+				},
+				{
+					"<leader>f.",
+					function()
+						require("utils").push_cwd(vim.fn.expand("%:p:h"))
+					end,
+					desc = "cwd to current file",
+				},
+				{
+					"<leader>fW",
+					function()
+						require("utils").pop_cwd()
+					end,
+					desc = "cwd back",
+				},
 			}
 		end,
 		config = function()
--- lua/utils.lua
+++ lua/utils.lua
@@ -1,5 +1,24 @@
 local M = {}
 
+M._root_cwd = vim.fn.getcwd()
+M._cwd_history = {}
+
+function M.push_cwd(dir)
+	table.insert(M._cwd_history, vim.fn.getcwd())
+	vim.fn.chdir(dir)
+	vim.notify("cwd → " .. dir, vim.log.levels.INFO)
+end
+
+function M.pop_cwd()
+	if #M._cwd_history == 0 then
+		vim.notify("cwd history is empty", vim.log.levels.WARN)
+		return
+	end
+	local prev = table.remove(M._cwd_history)
+	vim.fn.chdir(prev)
+	vim.notify("cwd ← " .. prev, vim.log.levels.INFO)
+end
+
 function M.telescope(builtin)
 	return function()
 		require("telescope.builtin")[builtin]()
